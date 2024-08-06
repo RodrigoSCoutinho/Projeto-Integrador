cb07d5e30f8571800013982ba317601dbad3c4d3
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2024-08-05 22:14:59 -0300

    feat: adding folder backend

---
+++ backend/main.py
@@ -0,0 +1,4 @@
+# //* Aqui vai ser o arquivo onde vamos escrever o código principal da nossa aplicação backend pegando os dados do frontend e fazendo a comunicação com o banco de dados.
+
+
+print("Hello World")
