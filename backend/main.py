219a156e1ae933e944a64eee55f4d8a1df2498ef
Rodrigo Coutinho <rodrigocoutinhojj29@gmail.com>
2024-09-03 18:25:26 -0300

    remove python backend

--- backend/main.py
+++
@@ -1,4 +0,0 @@
-# //* Aqui vai ser o arquivo onde vamos escrever o código principal da nossa aplicação backend pegando os dados do frontend e fazendo a comunicação com o banco de dados.
-
-
-print("Hello World")
