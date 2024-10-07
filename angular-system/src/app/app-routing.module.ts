6cd6afc1a3f699884f95ce355b517ae446f8c3e2
Rodrigo Coutinho <rodrigocoutinhojj29@gmail.com>
2024-10-06 23:25:35 -0300

    protecting the routes through with token

--- angular-system/src/app/app-routing.module.ts
+++ angular-system/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './componentes/login/login.component';
 import { MainComponent } from './componentes/main/main.component';
+import { authGuard } from './auth.guard';
 
 const routes: Routes = [
   {path: "", component: LoginComponent},
-  {path: "main", component: MainComponent }
+  {path: "main", component: MainComponent, canActivate: [authGuard]},
+
 ];
 
 @NgModule({
