7735264d5008779df8e6346bb0f871c1bc003e48
Abner Nascimento <abnert009@gmail.com>
2024-10-14 13:13:59 -0300

    feat:adding responsiveness and a hamburger menu

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Footer from './components/Footer'
 import Hero from './components/Hero'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div className=' text-white max-w-screen-xl mx-auto'>
-      
+    <div className=' text-white overflow-x-hidden'>
+      <Navbar/>
       <Hero/>
       <Footer/>
     </div>
--- frontend-react/tailwind.config.js
+++ frontend-react/tailwind.config.js
@@ -5,27 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      animation: {
-        fade: 'fadeOut 2s ease',  // Inclui fadeOut
-        slideIn: 'slideIn 1s ease',  // Inclui slideIn
-        slideTop: 'slideTop 1s ease',  // Inclui slideIn
-      },
-      keyframes: {
-        fadeOut: {
-          '0%': { opacity: '0' },  // Começa invisível
-          '100%': { opacity: '1' },  // Termina visível
-        },
-        slideIn: {
-          '0%': { transform: 'translateX(100%)', opacity: '0' },  // Começa fora da tela (à direita)
-          '100%': { transform: 'translateX(0)', opacity: '1' },   // Termina na posição original
-        },
-        slideTop: {
-          '0%': { transform: 'translateY(100%)', opacity: '0' },  // Começa fora da tela (à direita)
-          '100%': { transform: 'translateY(0)', opacity: '1' },   // Termina na posição original
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [
     require("daisyui")
